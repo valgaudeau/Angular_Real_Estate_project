aadafeae7d9eb1bf6526b3189c4f72129fafbd1f
valgaudeau <valgaudeau@outlook.com>
2022-10-22 21:31:17 +0100

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'real-estate'`, () => {
+  it(`should have as title 'real_estate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('real-estate');
+    expect(app.title).toEqual('real_estate');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('real-estate app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('real_estate app is running!');
   });
 });
---
+++ src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  // template: ``,
+  templateUrl: 'property-card.component.html',
+  // styles:  ['h1{font-weight: normal;}']
+  styleUrls: ['property-card.component.css']
+}
+
+)
+export class PropertyCardComponent{
+
+}
