02cb5fe5d502394064d8f66ebf2f2c969b59afb0
valgaudeau <valgaudeau@outlook.com>
2022-10-23 12:24:54 +0100

    Displaying multiple cards and passing data from property list

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -10,10 +10,5 @@ import { Component } from '@angular/core';
 
 )
 export class PropertyCardComponent{
-  Property: any = {
-    "Id":1,
-    "Name":"Birla House",
-    "Type":"House",
-    "Price":12000
-  }
+  @Input() estate: any
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Erose Villa",
+      "Type":"House",
+      "Price":11400
+    },
+    {
+      "Id":3,
+      "Name":"placeholder 3",
+      "Type":"House",
+      "Price":15000
+    },
+    {
+      "Id":4,
+      "Name":"placeholder 4",
+      "Type":"House",
+      "Price":16000
+    },
+    {
+      "Id":5,
+      "Name":"placeholder 5",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":6,
+      "Name":"placeholder 6",
+      "Type":"House",
+      "Price":12000
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
