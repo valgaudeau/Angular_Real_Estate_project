6581e3df537b3b0e8e3259a551c1d58a7beadffc
valgaudeau <valgaudeau@outlook.com>
2022-10-23 10:51:17 +0100

    Created nav-bar and started working on the property cards

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -10,5 +10,10 @@ import { Component } from '@angular/core';
 
 )
 export class PropertyCardComponent{
-
+  Property: any = {
+    "Id":1,
+    "Name":"Birla House",
+    "Type":"House",
+    "Price":12000
+  }
 }
