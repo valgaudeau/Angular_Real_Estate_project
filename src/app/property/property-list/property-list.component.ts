62ededff09448632137c2da8314119d51dd76b52
valgaudeau <valgaudeau@outlook.com>
2022-10-24 00:26:32 +0100

    Created Service to provide property list data

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -10,15 +10,23 @@ export class PropertyListComponent implements OnInit {
 
   properties: any;
 
-  constructor(private http:HttpClient) { }
+  constructor(private housingService: HousingService) { }
 
   ngOnInit(): void {
-    this.http.get('./assets/properties.json').subscribe(
-      data=>{
+    this.housingService.getAllProperties().subscribe(
+        data=>{
         this.properties = data;
         console.log(data)
+      }, error => {
+        console.log(error);
       }
-    );
+    )
+    // this.http.get('./assets/properties.json').subscribe(
+    //   data=>{
+    //     this.properties = data;
+    //     console.log(data)
+    //   }
+    // );
   }
 
 }
