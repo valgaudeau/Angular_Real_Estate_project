9ee0961710c5efbaa2179b23416d4c16520a5b52
valgaudeau <valgaudeau@outlook.com>
2022-10-24 23:31:50 +0100

    More routing work & Added propertyDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { PropertyListComponent } from './property/property-list/property-list.co
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HousingService } from './services/housing.service';
 import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
 
 const appRoutes: Routes = [
   {path: '', component: PropertyListComponent},
-  {path: 'add-property', component: AddPropertyComponent}
+  {path: 'rent-property', component: PropertyListComponent},
+  {path: 'add-property', component: AddPropertyComponent},
+  {path: 'property-detail/:id', component: PropertyDetailComponent}
 ]
 
 @NgModule({
@@ -21,7 +24,8 @@ const appRoutes: Routes = [
     PropertyCardComponent,
     PropertyListComponent,
       NavBarComponent,
-      AddPropertyComponent
+      AddPropertyComponent,
+      PropertyDetailComponent
    ],
   imports: [
     BrowserModule,
