f7a93ea45ff2b15a4f7e3d77cd1cccf4d49d5e7b
valgaudeau <valgaudeau@outlook.com>
2022-10-27 11:46:46 +0100

    Changed property card type from any to IProperty

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-card',
@@ -10,5 +11,5 @@ import { Component, Input } from '@angular/core';
 
 )
 export class PropertyCardComponent{
-  @Input() estate: any
+  @Input() property: IProperty | undefined;
 }
