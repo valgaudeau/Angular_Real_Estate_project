fa71d88519a7fd927c76891d0fde8461f738d308
valgaudeau <valgaudeau@outlook.com>
2022-10-27 14:46:12 +0100

    Split the 'BUY' and 'RENT' property views. Used ActivatedRoute to retrieve url snapshot, and different arrays + ngIf in the HTML

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -9,25 +9,46 @@ import { IProperty } from '../IProperty.interface';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  sellRent = 1;
-  properties: IProperty[] = [];
+  sellOrRentRoute = 1; // by default, we will will show the properties available for purchase. If value = 2, then we have to display properties up for rent
+  allProperties: IProperty[] = [];
+  buyProperties: IProperty[] = [];
+  rentProperties: IProperty[] = [];
   errorMessage: string = '';
 
   constructor(private route: ActivatedRoute, private housingService: HousingService) { }
 
   ngOnInit(): void {
-    if(this.route.snapshot.url.toString())
+    if(this.route.snapshot.url.toString()) // this gives us the current path of the route. We need this to identify is we are on the rent-property path to display the appropriate properties.
     {
-      this.sellRent = 2; // we are on rent-property URL
+      this.sellOrRentRoute = 2; // we are on rent-property URL
     }
     // We call the subscribe method passing in an observer object. The observer object provides functions to react to the different types of notifications we can receive
     // from an observer. Those 3 notifications are next, error, and complete.
     // next allows us to specify what we want to do when the observable emits the next value. Since this is an HTTP request, it only emits one time.
     // error allows us to define what to do if the observable emits an error.
     this.housingService.getAllProperties().subscribe({
-      next: properties => this.properties = properties,
+      next: properties => this.allProperties = properties,
       error: err => this.errorMessage = err
     })
+
+    this.housingService.getAllProperties().subscribe({
+      next: properties => this.buyProperties = properties.filter(property => property.SellRent == 1),
+      error: err => this.errorMessage = err
+    })
+
+    this.housingService.getAllProperties().subscribe({
+      next: properties => this.rentProperties = properties.filter(property => property.SellRent == 2),
+      error: err => this.errorMessage = err
+    })
+
+    // console.log("HI FROM ngOnInit in property list component");
+
+    // DONT KNOW WHY BUT THIS DOESNT WORK - DOESNT SHOW UP IN THE CONSOLE, AND HAVING THE SEPARATEPROPERTIES FUNCTION DOESN'T DO THE JOB
+    // for(let property of this.allProperties){
+    //   console.log(property.Name + " hey this is property in ngOnInit");
+    // }
+
+    // this.separatePropertiesByType();
 
     // this.housingService.getAllProperties().subscribe(
     //     data=>{
@@ -38,5 +59,21 @@ export class PropertyListComponent implements OnInit {
     //   }
     // )
   }
+
+  // Need a method here which we can call in ngOnInit() that will fill the buyProperties and rentProperties arrays
+  // separatePropertiesByType(){
+  //   for(let property of this.allProperties){
+  //     if(property.SellRent == 1)
+  //     {
+  //       this.buyProperties.push(property);
+  //       console.log(property + " added to buyProperty array");
+  //     }
+  //     else
+  //     {
+  //       this.rentProperties.push(property);
+  //       console.log(property + " added to rentProperty array");
+  //     }
+  //   }
+  // }
 
 }
