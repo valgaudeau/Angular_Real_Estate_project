cf81563e75aa26e474ab78f223462c561c61f03b
valgaudeau <valgaudeau@outlook.com>
2022-10-27 11:44:37 +0100

    Changed the housing service observable function & the property list which subscribes to it

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-list',
@@ -9,28 +10,33 @@ import { HousingService } from 'src/app/services/housing.service';
 })
 export class PropertyListComponent implements OnInit {
   sellRent = 1;
-  properties: any;
+  properties: IProperty[] = [];
+  errorMessage: string = '';
 
   constructor(private route: ActivatedRoute, private housingService: HousingService) { }
 
   ngOnInit(): void {
-    if(this.route.snapshot.url.toString()){
+    if(this.route.snapshot.url.toString())
+    {
       this.sellRent = 2; // we are on rent-property URL
     }
-    this.housingService.getAllProperties().subscribe(
-        data=>{
-        this.properties = data;
-        console.log(data)
-      }, error => {
-        console.log(error);
-      }
-    )
-    // this.http.get('./assets/properties.json').subscribe(
-    //   data=>{
+    // We call the subscribe method passing in an observer object. The observer object provides functions to react to the different types of notifications we can receive
+    // from an observer. Those 3 notifications are next, error, and complete.
+    // next allows us to specify what we want to do when the observable emits the next value. Since this is an HTTP request, it only emits one time.
+    // error allows us to define what to do if the observable emits an error.
+    this.housingService.getAllProperties().subscribe({
+      next: properties => this.properties = properties,
+      error: err => this.errorMessage = err
+    })
+
+    // this.housingService.getAllProperties().subscribe(
+    //     data=>{
     //     this.properties = data;
     //     console.log(data)
+    //   }, error => {
+    //     console.log(error);
     //   }
-    // );
+    // )
   }
 
 }
--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,15 +1,50 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { IProperty } from '../property/IProperty.interface';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Service is registered in the root, meaning it can be accessed from any component or service in the application. Alternative = component injector.
 })
 export class HousingService {
 
+private propertyUrl = './assets/properties.json';
+
 constructor(private http:HttpClient) { }
 
+// Notes on the functions used below:
+// The tap operator allows to access the item emitted by the Observable without modifying it. This operator takes in an arrow function. The parameter 'data' is the emitted data,
+// and the arrow function is what we want to do with it. So right now we have (data => console.log('All', JSON.stringify(data))) to log all of the emitted data to the console.
+// Next, we have the catchError function. Often the emitted data will have errors, and we want our pipe to catch these issues.
+getAllProperties() : Observable<IProperty[]>
+{
+  return this.http.get<IProperty[]>(this.propertyUrl).pipe(
+    tap(data => console.log('All', JSON.stringify(data))),
+    catchError(this.handleError)
+  );
+}
+
+/*
 getAllProperties(){
   return this.http.get('./assets/properties.json')
+}
+*/
+
+private handleError(err: HttpErrorResponse){
+  let errorMessage = '';
+  if(err.error instanceof ErrorEvent)
+  {
+    // If a client-side or network error occurred, we can handle it here
+    errorMessage = `An error occurred: ${err.error.message}`;
+  }
+  else
+  {
+    // If we reach this section, the backend returned an unsuccessful response code.
+    // The response body returned by the server should contain clues as to what went wrong.
+    errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+  }
+  console.error(errorMessage);
+  return throwError(()=>errorMessage);
 }
 
 }
