4b7f8700ea9337467dc214829038309a121c5a91
valgaudeau <valgaudeau@outlook.com>
2022-10-28 10:53:48 +0100

    Created userLogin and userRegister components & added navigation

---
+++ src/app/user/user-login/userLogin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userLogin',
+  templateUrl: './userLogin.component.html',
+  styleUrls: ['./userLogin.component.css']
+})
+export class UserLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/user/user-register/userRegister.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-userRegister',
+  templateUrl: './userRegister.component.html',
+  styleUrls: ['./userRegister.component.css']
+})
+export class UserRegisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
