f4b137c5358c27e88d4ce7fdec5a87248e51af03
valgaudeau <valgaudeau@outlook.com>
2022-10-30 22:25:13 +0000

    userName invalid not triggering - Not sure why

--- src/app/user/user-register/userRegister.component.ts
+++ src/app/user/user-register/userRegister.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-userRegister',
@@ -8,11 +8,16 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 })
 export class UserRegisterComponent implements OnInit {
 
-  // registrationForm!: FormGroup;
-  constructor() { }
+  // registrationForm: FormGroup;
+  // user: any = { } // the open brackets here signal that this is just a blank JS object. We can fill it with the values we receive from our registration form in the onSubmit method
+  registrationForm = new FormGroup({
+    userName: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    email: new FormControl("", [Validators.required, Validators.email]), // we pass an array of validators
+    password: new FormControl("", [Validators.required, Validators.minLength(8)])
+  })
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
-
     // this.registrationForm = new FormGroup({
     //   userName: new FormControl('Mark', Validators.required),
     //   email: new FormControl(null, [Validators.required, Validators.email]), // Here we use an array of validators passed in the second argument. This is how we have multiple validators with Reactive Forms
@@ -26,9 +31,13 @@ export class UserRegisterComponent implements OnInit {
   //     return fg.get('password').value === fg.get('confirmPassword').value ? { notmatched: false } : { notmatched: true };
   // }
 
-  onSubmit(form : NgForm){
-    console.log(form);
-    // console.log(this.registrationForm);
+  get userName(){
+    return this.registrationForm.get('user');
+  }
+
+  onSubmit(){
+    // console.log(form);
+    console.log(this.registrationForm.value);
   }
 
 }
