34c725abf44ab53b4de26e5ea60c0bcb22a3ff6b
valgaudeau <valgaudeau@outlook.com>
2022-10-31 17:12:07 +0000

    Created logic to save user data to local storage

--- src/app/user/user-register/userRegister.component.ts
+++ src/app/user/user-register/userRegister.component.ts
@@ -10,6 +10,7 @@ export class UserRegisterComponent implements OnInit {
 
   registrationForm!: FormGroup;
   submitted = false;
+  user : any = { };
 
   constructor(private fb: FormBuilder) { }
 
@@ -32,7 +33,6 @@ export class UserRegisterComponent implements OnInit {
 
   onSubmit(){
     // console.log(form);
-    // console.log(this.registrationForm.value);
     this.submitted = true;
 
     // if the form is invalid, we don't do anything just return
@@ -40,7 +40,26 @@ export class UserRegisterComponent implements OnInit {
       return;
     }
 
+    console.log(this.registrationForm.value);
+    this.user = Object.assign(this.user, this.registrationForm.value);
+    // localStorage.setItem('Users', JSON.stringify(this.user));
+    this.addUser(this.user);
     alert("Success");
+  }
+
+  // To make TypeScript tolerate parameters without declare type, you can edit the tsconfig.json file, see https://stackoverflow.com/questions/47848778/parameter-implicitly-has-an-any-type.
+  // Instead I just declared user type as any here in the parameter of this function
+  addUser(user : any){
+    let userList = [];
+    if(localStorage.getItem('Users')){
+      // we need an or condition on the code line below because localStorage.getItem('Users') can return a null, and JSON.parse function only takes a string arg.
+      // See https://stackoverflow.com/questions/46915002/argument-of-type-string-null-is-not-assignable-to-parameter-of-type-string
+      userList = JSON.parse(localStorage.getItem('Users') || '{}');
+      userList = [user, ...userList];
+    } else {
+      userList = [user];
+    }
+    localStorage.setItem('Users', JSON.stringify(userList));
   }
 
 }
