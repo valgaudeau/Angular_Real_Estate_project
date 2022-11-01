50384dccb2e4eb00e4619b21e6528fd3601a82fe
valgaudeau <valgaudeau@outlook.com>
2022-11-01 18:04:06 +0000

    more work on UserLoginComponent

--- src/app/user/user-login/userLogin.component.ts
+++ src/app/user/user-login/userLogin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+import * as alertify from 'alertifyjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-userLogin',
@@ -9,18 +11,24 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class UserLoginComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   onLogin(loginForm: NgForm){
     console.log(loginForm);
-    const user = this.authService.authenticateUser(loginForm.value);
-    if(user){
-      console.log('Login was successful');
+    const token = this.authService.authenticateUser(loginForm.value);
+    if(token){
+      localStorage.setItem('token', token['userName']);
+      // console.log('Login was successful');
+      alertify.set("notifier", "position", "top-center");
+      alertify.success("You have successfully logged in!");
+      this.router.navigate(['/']); // upon successful login, we redirect our users to the front page of the app
     } else {
-      console.log('Login was not successful');
+      // console.log('Login was not successful');
+      alertify.set("notifier", "position", "top-center");
+      alertify.error("Error encountered, please make sure that you are registered or that your credentials are correct");
     }
   }
 
