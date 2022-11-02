470f4d3dc1946567cad4f628863d7d1a35f98182
valgaudeau <valgaudeau@outlook.com>
2022-11-02 12:25:51 +0000

    Implemented 'Next' button functionality for tabs

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-add-product',
@@ -11,6 +12,10 @@ export class AddProductComponent implements OnInit {
 
   @ViewChild('Form')
   addProductForm!: NgForm;
+
+  @ViewChild('formTabs', { static: false })
+  formTabs?: TabsetComponent;
+
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -29,6 +34,13 @@ export class AddProductComponent implements OnInit {
   onSubmit(){
     // console.log(form);
     console.log(this.addProductForm);
+  }
+
+  // See https://valor-software.com/ngx-bootstrap/#/components/tabs?tab=overview - Documentation for all of this
+  selectTab(tabId: number) {
+    if (this.formTabs?.tabs[tabId]) {
+      this.formTabs.tabs[tabId].active = true;
+    }
   }
 
 }
