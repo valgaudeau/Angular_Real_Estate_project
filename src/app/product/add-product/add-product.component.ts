510897962597625e43ce4b139d39f9d274d5e03f
valgaudeau <valgaudeau@outlook.com>
2022-11-04 14:51:07 +0000

    Trying to save without tab1 fields = back to tab1
    Now if the user submits a new product without having filled all of the fields in the first tab, he will notified through alertify that he needs to do that, and the view will shift to that tab.

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
+import * as alertify from 'alertifyjs'
 
 @Component({
   selector: 'app-add-product',
@@ -28,12 +29,43 @@ export class AddProductComponent implements OnInit {
     // }, 100)
   }
 
+  // Using these getters, we can retrieve information about the status FormGroup controls in the HTML
+  get basicInfo() {
+    return this.addProductForm.controls['BasicInfo'] as FormGroup;
+  }
+
+  get extraInfo() {
+    return this.addProductForm.controls['ExtraInfo'] as FormGroup;
+  }
+
   onBack()
   {
     this.router.navigate(['/'])
   }
 
   onSubmit(){
+    // If the invalid error comes from group 1, we need to move to that tab. We can use the getters to retrieve the status
+    // error comes from first tab, move view to first tab
+    if(this.basicInfo.invalid) {
+      if(this.formTabs){
+        this.formTabs.tabs[0].active = true;
+        alertify.set("notifier", "position", "top-center");
+        alertify.error("You need to fill in all of the fields on this tab before you can save your product!")
+        return;
+      }
+    }
+
+    // if error comes from the last tab, move view to last tab
+    // Don't need this one actually since if on last tab, can only save if all fields on the tab are filled & valid
+    // if(this.extraInfo.invalid) {
+    //   if(this.formTabs){
+    //     this.formTabs.tabs[2].active = true;
+    //     alertify.set("notifier", "position", "top-center");
+    //     alertify.error("You need to fill in all of the fields on this tab before you can save your product!")
+    //     return;
+    //   }
+    // }
+
     // console.log(form);
     console.log("The form has been submitted");
     console.log('spaceship or robot = ' + this.addProductForm.value.BasicInfo.spaceshipOrRobot);
@@ -47,6 +79,7 @@ export class AddProductComponent implements OnInit {
     }
   }
 
+  // see https://www.cloudhadoop.com/angular-number-validation-example/ for this function - Number validation in Angular
   onKeyPress(event: any) {
     const regexpNumber = /[0-9\+\-\ ]/;
     let inputCharacter = String.fromCharCode(event.charCode);
