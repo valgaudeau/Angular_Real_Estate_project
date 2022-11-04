07e726143802a5e5f25e71c73fa15fe1c0cbe385
valgaudeau <valgaudeau@outlook.com>
2022-11-04 12:06:58 +0000

    Form only allows number input for Age & Price

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -47,4 +47,12 @@ export class AddProductComponent implements OnInit {
     }
   }
 
+  onKeyPress(event: any) {
+    const regexpNumber = /[0-9\+\-\ ]/;
+    let inputCharacter = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !regexpNumber.test(inputCharacter)) {
+      event.preventDefault();
+    }
+  }
+
 }
