de9f879fc37332848f9f5b9b2f6cb237d1c6be1e
valgaudeau <valgaudeau@outlook.com>
2022-11-07 19:13:01 +0000

    Refactored product list component code
    It now has only 1 array containing products, and the logic of what is displayed is based on the route snapshot. I think this is cleaner, and also it fixes the issue I was having with the search bar not working for spaceships & robot views when I had different arrays.

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -11,42 +11,38 @@ import { IProduct } from '../IProduct.interface';
 export class ProductListComponent implements OnInit {
   spaceshipOrRobot = 0; // by default, we show ALL of the products. If value = 1, we show spaceships. If value = 2, we show robots
   allProducts: IProduct[] = [];
-  allSpaceships: IProduct[] = [];
-  allRobots: IProduct[] = [];
   errorMessage: string = '';
   searchText: string = ''; // when the searchTextChanged event is raised, we set the value of this property
 
   constructor(private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit(): void {
-    if(this.route.snapshot.url.toString() == 'spaceships') // this gives us the current path of the route. We need this to identify is we are on the robot path to display the appropriate properties.
-    {
-      // console.log("yes this is the route.snapshot.url value for spaceship page " + this.route.snapshot.url.toString());
-      this.spaceshipOrRobot = 1; // we are on rent-property URL
-    }
-
-    if(this.route.snapshot.url.toString() == 'robots') // this gives us the current path of the route. We need this to identify is we are on the robot path to display the appropriate properties.
-    {
-      // console.log("yes this is the route.snapshot.url value for robot page " + this.route.snapshot.url.toString());
-      this.spaceshipOrRobot = 2; // we are on rent-property URL
-    }
+    console.log(this.route.snapshot.url.toString()); // route.snapshot.url gives us the current path of the route
     // We call the subscribe method passing in an observer object. The observer object provides functions to react to the different types of notifications we can receive from an observer. Those 3 notifications are next, error, and complete.
     // next allows us to specify what we want to do when the observable emits the next value. Since this is an HTTP request, it only emits one time.
     // error allows us to define what to do if the observable emits an error.
-    this.productService.getAllProducts().subscribe({
-      next: products => this.allProducts = products,
-      error: err => this.errorMessage = err
-    })
-
-    this.productService.getAllProducts().subscribe({
-      next: products => this.allSpaceships = products.filter(products => products.SpaceshipOrRobot == 1),
-      error: err => this.errorMessage = err,
-    })
-
-    this.productService.getAllProducts().subscribe({
-      next: products => this.allRobots = products.filter(products => products.SpaceshipOrRobot == 2),
-      error: err => this.errorMessage = err
-    })
+    if(this.route.snapshot.url.toString() == 'spaceships') // spaceships in the url
+    {
+      this.productService.getAllProducts().subscribe({
+        next: products => this.allProducts = products.filter(products => products.SpaceshipOrRobot == 1),
+        error: err => this.errorMessage = err,
+      })
+      // console.log("yes this is the route.snapshot.url value for spaceship page " + this.route.snapshot.url.toString());
+      this.spaceshipOrRobot = 1; // we are on rent-property URL
+    } else if (this.route.snapshot.url.toString() == 'robots') // robot in the url
+    {
+      this.productService.getAllProducts().subscribe({
+        next: products => this.allProducts = products.filter(products => products.SpaceshipOrRobot == 2),
+        error: err => this.errorMessage = err
+      })
+      // console.log("yes this is the route.snapshot.url value for robot page " + this.route.snapshot.url.toString());
+      this.spaceshipOrRobot = 2; // we are on rent-property URL
+    } else {
+      this.productService.getAllProducts().subscribe({
+        next: products => this.allProducts = products,
+        error: err => this.errorMessage = err
+      })
+    }
 
   }
 
