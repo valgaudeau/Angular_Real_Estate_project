efdd11f8766538eeb51fbf1def335f84f88efff8
valgaudeau <valgaudeau@outlook.com>
2022-11-07 22:48:34 +0000

    Added Timeout for search box
    It was searching instantly upon user input in the search box, which felt weird. I added a small timeout of 200ms to address this. Can play around with this of course.

--- src/app/product-list-search/product-list-search.component.ts
+++ src/app/product-list-search/product-list-search.component.ts
@@ -20,7 +20,10 @@ export class ProductListSearchComponent implements OnInit {
   // This function will be called when the search text box input field is changed. At that point, whatever the user has entered in the search box will be assigned to the enteredSearchValue property.
   // This function needs to be called whenever the user types in the text box. For that, we need to bind the input event (which is triggered when user enters stuff in text box) with this function.
   onSearchTextChanged(){
-    this.searchTextChanged.emit(this.enteredSearchValue);
+    // Right now I feel like this is happening to quickly. Added Timeout here with a lambda expression since first parameter has to be function. See https://www.educba.com/settimeout-typescript/
+    setTimeout(() => {
+      this.searchTextChanged.emit(this.enteredSearchValue);
+    }, 200);
   }
 
 }
